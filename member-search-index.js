7b16bc34af0e40d7f40756f0799c96c853b3e568
Alvaro Fernandez <alvaro.619@hotmail.es>
2022-11-28 09:42:41 +0100

    Inicial

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"practicadocu","c":"Empleado","l":"Empleado()","u":"%3Cinit%3E()"},{"p":"practicadocu","c":"Empleado","l":"Empleado(String, String, int, double)","u":"%3Cinit%3E(java.lang.String,java.lang.String,int,double)"},{"p":"practicadocu","c":"Empleado","l":"plus(double)"}];updateSearchResults();
